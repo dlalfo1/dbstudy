e19bf44d66f84b1d385fed5396ee20c4ff5d4a49
dlalfo1 <mirae1213@daum.net>
2023-02-02 17:17:46 +0900

    2023-02-02

---
+++ Day01_02_테이블만들기.sql
@@ -0,0 +1,80 @@
+/*
+    테이블
+    1. 데이터베이스의 가장 대표적인 객체이다.
+    2. 데이터를 보관하는 객체이다.
+    3. 표 형식으로 데이터를 보관한다.
+    4. 테이블 기본 용어
+        1) 열 : colum, 속성(attribute), 필드(field) 
+        2) 행 : row,   개체(entity),    레코드(record) - 데이터를 의미한다.
+*/
+
+/* 
+    오라클의 데이터 타입
+    1. CHAR(size)    : 고정 길이 문자 타입(size : 1 ~ 2000바이트)
+        - 오라클에선 한글자든 여러글자든 나눠서 저장하지 않는다. (JAVA에서 String개념이라고 생각하면 된다.)
+        - 길이가 고정적일 때 사용한다(ex. 주민등록번호)
+    2. VARCHAR2(size) : 가변길이 문자 타입(size : 1~ 4000바이트)    
+        - 길이가 다 다를 때 사용한다 (ex. 집 주소)
+    3. DATE          : 날짜/시간 타입(JAVA DATE 클래스와 매칭하기 위한 타입)
+    4. TIMESTAMP     : 날짜/시간 타입(좀 더 정밀) - (JAVA TIMESTAMP 클래스와 매칭하기 위한 타입)
+    5. NUMBER(p,s)   : 정밀도(p), 스케일(s)로 표현하는 숫자 타입
+        1) 정밀도 : 정수부와 소수부를 모두 포함하는 전체 유효 숫자가 몇 개인가?
+        3) 스케일 : 소수부의 전체 유효 숫자가 몇 개인가?
+        예시)
+            (1) NUMBER      : 최대 38자리 숫자(22바이트) - 소숫점이 들어갈 수 있는가
+            (2) NUMBER(3)   : 정수부가 최대 3자리인 숫자(최대 999)
+            (3) NUMBER(5,2) : 최대 전체 5자리, 소수부 2자리인 숫자 ex)23.45
+            (4) NUMBER(2,2) : 1 미만의 소수부 2자리인 실수 ex)0.15 - 정수부의 0은 유효 자리수가 아니다. 0이 소수부 뒤에 붙어도 그러하다.
+       -> JAVA에서는 int, double 등등으로 정수 실수를 구분했지만 Oracle SQL에선 NUMBER로 구분한다.  
+*/
+
+/*
+    제약조건(Constraint) - 5개 중 3개 
+    1. 널
+        1) NULL 또는 생략
+        2) NOT NULL
+    2. 중복 데이터 방지 - 데이터가 중복이 되면 안 될 때 사용한다. ex) 주민등록번호
+        UNIQUE 
+    3. 값의 제한 - 특정한 값이 들어가면 안될 때 사용한다. 값의 범위를 지정해준다고 생각하면 될 것 같다.
+        CHECK
+        
+    
+*/
+
+-- 예시 테이블
+-- SQL에선 변수이름을 앞에 두고 타입을 뒤에 둔다.
+-- 키워드 DROP, CREATE - 뭔가를 지우고 만들 때 쓴다.(DBL)  
+-- 쿼리문을 쓸 때 대문자든 소문자든 상관이 없다. 다만 한가지로 통일해야 한다. (무엇으로 쓰든 DB로 넘어갈 때 대문자로 바뀐다.)
+-- 키워드 입력할 때 순서는 상관이 없다. 띄어쓰기만 해서 써주면 된다.
+-- = 키워드는 같다이다.
+-- 글자수에 상관없이 '' 작은따옴표로 묶는다.
+-- 그리고 AND , 또는 OR
+DROP TABLE PRODUCT;
+CREATE TABLE PRODUCT(
+    CODE         VARCHAR2(2 BYTE)  NOT NULL UNIQUE, -- CODE의 값은 NULL일 수 없다. NULLABLE에 NO가 표시된다. 제약조건은 UNIQUE이다.
+    MODEL        VARCHAR2(10 BYTE) NULL, 
+    CATEGORY     VARCHAR2(5 BYTE)  CHECK(CATEGORY= 'MAIN' OR CATEGORY = 'SUB'), -- CHECK(CATEGORY IN('MAIN', 'SUB')) : CATEGORY가 MAIN과 SUB중 하나다. OR을 쓰는건 현실적으로 없다.
+    PRICE        NUMBER            CHECK(PRICE >= 0), 
+    AMOUNT       NUMBER(2)         CHECK(AMOUNT >= 0 AND AMOUNT <= 100),        -- CHECK(AMOUNT BETWEEN 0 AND 100) : AMOUNT가 0과 100사이이다.(0과 100 포함)
+    MANUFACTURED DATE
+);
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ Day01_03_테이블조회하기.sql
@@ -0,0 +1,134 @@
+/*
+    DQL
+    1. Data Query Language
+    2. 데이터 질의(조회) 언어
+    3. 테이블의 데이터를 조회하는 언어이다.
+    4. 테이블 내용의 변경이 생기지 않는다. - 트랜잭션의 대상이 아니고, COMMIT이 필요하지 않다.
+    5. 형식 ([]는 생략 가능) 
+        SELECT 조회할칼럼, 조회할칼럼, 조회할칼럼, ... 
+        FROM 테이블이름 (SELECT절, FROM절) - 각 절별로 한줄씩 쓰는 것을 권장한다.
+        [WHERE 조건식] 
+        [GROUP BY 그룹화할칼럼 [HAVING 그룹조건식] - HAVING은 GROUP BY가 나와야 사용 가능하다.
+        [ORDER BY 정렬할칼럼 정렬방식]   
+    6. 순서 (FROM -> WHERE -> SELECT-> ORDER) - 실제 쿼리를 적을 때 쓰는 순서가 아니라 처리되는 순서이다.
+        ④ SELECT 조회할칼럼, 조회할칼럼, 조회할칼럼, ... 
+        ① FROM 테이블이름   
+        ② [WHERE 조건식] 
+        ③ [GROUP BY 그룹화할칼럼 [HAVING 그룹조건식]
+        ⑤ [ORDER BY 정렬할칼럼 정렬방식]   
+/*
+
+/*
+    트랜잭션
+    1. Transaction
+    2. 데이터베이스의 상태를 변화시키기 위해 수행하는 작업 단위
+    3. 여러 개의 세부 작업으로 구성된 하나의 작업을 의미한다. 
+    4. 모든 세부 작업이 성공하면 COMMIT, 하나라도 실패하면 모든 세부 작업의 취소를 진행한다.
+        (All of Nothing) 
+        - ex) 휴먼고객처리시 정상테이블에서 지우고 휴먼테이블에 넣어야하는 작업을 둘 다 해야하는데 
+              둘 중 하나만 하게 된다면 데이터의 훼손이 발생하므로
+              그런 상황을 막기 위해서 두 작업 모두 하고 COMMIT을 해야한다. 하나만 한다면 모든 작업을 취소한다.
+*/
+
+-- 조회 실습.
+-- 1. 사원 테이블에서 사원명 조회하기
+-- 1) 기본 방식
+SELECT ENAME -- SELECT의 결과는 테이블이다.
+  FROM EMP;
+
+-- 2) 오너 명시하기(테이블을 가지고 있는 계정)
+SELECT ENAME
+  FROM SCOTT.EMP;  
+
+-- 3) 테이블 명시하기(칼럼을 가지고 있는 테이블)
+SELECT EMP.ENAME
+  FROM EMP; -- SELECT에서 테이블을 명시해줬더라도 FROM은 생략할 수 없다.
+
+-- 4) 테이블 별명 지정하기
+SELECT E.ENAME -- 그럼 칼럼에서 별명으로 부를 수 있다. AS(ALIAS)를 사용할 수 없다.
+  FROM EMP E; -- EMP 테이블의 별명을 E로 부여한다.
+
+--5) 칼럼 별명 지정하기
+SELECT E.ENAME AS 사원명 -- E.ENAME 칼럼의 별명을 '사원명'으로 부여한다. AS(ALIAS)를 사용할 수 있다.
+  FROM EMP E;
+
+-- 2. 사원테이블의 모든 칼럼 조회하기
+-- 1) * 활용하기(SELECT절에서 *는 모든 칼럼을 의미한다.)
+SELECT *       -- 불려 가기 싫으면 사용 금지!   
+  FROM EMP;
+
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO 
+  FROM EMP;
+
+-- 3. 동일한 데이터는 한 번만 조회하기
+SELECT DISTINCT JOB -- DISTINCT 중복값 제거하기 (JOB 칼럼 가져올 때 중복된 값을 제거해준다.)  
+ FROM EMP;
+
+-- 4. JOB이 MANAGER인 사원 목록 조회하기
+
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE JOB = 'MANAGER'; -- 오라클에서 같다는 = 한개.
+ 
+ SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE JOB in('MANAGER'); -- in은 가져올 값이 여러개일 때 주로 사용한다. (1개도 사용가능하다.) 
+ 
+-- 5. SAL이 1500 초과인 사원 목록 조회하기
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE SAL > 1500;
+ 
+-- 6. SAL이 2000 ~ 2999인 사원 목록 조회하기
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE SAL BETWEEN 2000 AND 2999; 
+ 
+-- 7. COMM을 받는 사원 목록 조회하기
+--    1) NULL 이다 : IS NULL
+--    2_ NULL 아니다 : IS NOT NULL
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE COMM IS NOT NULL -- NULL 체크의 방법은 별도의 방법이 있다.
+   AND COMM != 0;       -- 0은 != 기호로 체크할 수 있다.   
+                                      
+-- 8. ENAME이 A로 시작하는 사원 목록 조회하기
+--    1) WILD CARD (보통 % 쓴다.)
+--       (1) % : 모든 문자, 글자 수 제한 없은 모든 문자
+--       (2) _ : 1글자로 제한된 모든 문자      
+--    2) 연산자
+--        (1) LIKE     : WID CARD를 포함한다.
+--        (2) NOT LIKE : WILD CARD를 포함하지 않는다.  
+/*
+    A% (A로 시작하는 모든 문자)     A_ (A로 시작하는 1글자)
+    AI                              AI
+    APP                             AP
+    APPLE                           AM    
+    
+    %A% (A가 포함된 모든 문자)     %A (A로 끝나는 모든문자)
+    AI                             HA
+    HAIVE                          AREA
+    GIANT                             
+    
+    보통 %A% 이렇게 특정 단어가 포함된 키워드를 많이 쓴다.
+*/
+
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+  FROM EMP
+ WHERE ENAME LIKE 'A%'; -- 와일드 카드 사용시 LIKE 또는 NOT LIKE 연산자를 사용해야 한다. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
